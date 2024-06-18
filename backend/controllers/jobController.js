980531c93e30601f73ae8a5478c1ada04f5ed748
ranjansatvik <satvikranjan1@gmail.com>
2024-06-18 17:28:49 +0530

    added job controller function

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -1,11 +1,76 @@
-import { catchAsyncError } from "../middlewares/catchAsyncError";
-import ErrorHandler from "../middlewares/error";
-import { Job } from "../models/jobSchema";
+import { catchAsyncError } from "../middlewares/catchAsyncError.js";
+import ErrorHandler from "../middlewares/error.js";
+import { Job } from "../models/jobSchema.js";
 
-const getAllJobs = catchAsyncError(async (req, res, text) => {
+export const getAllJobs = catchAsyncError(async (req, res, next) => {
   const jobs = await Job.find({ expired: false });
   res.status(200).json({
     success: true,
     jobs,
+  });
+});
+
+export const postJob = catchAsyncError(async (req, res, next) => {
+  const { role } = req.user;
+  if (role === "Job Seeker") {
+    return next(
+      new ErrorHandler("Job Seeker not allowed to access this resource.", 400)
+    );
+  }
+  const {
+    title,
+    description,
+    category,
+    country,
+    city,
+    location,
+    fixedSalary,
+    salaryFrom,
+    salaryTo,
+  } = req.body;
+
+  if (!title || !description || !category || !country || !city || !location) {
+    return next(
+      new ErrorHandler(
+        "Please provide full job details.", //basic job detail
+        400
+      )
+    );
+  }
+
+  if ((!salaryFrom || !salaryTo) && !fixedSalary) {
+    return next(
+      new ErrorHandler(
+        "Please either provide fixed salary or ranged salary.", // provided netiher details
+        400
+      )
+    );
+  }
+
+  if (salaryFrom && salaryTo && fixedSalary) {
+    return next(
+      new ErrorHandler(
+        "Cannot Enter Fixed and Ranged Salary together.", // provided all details
+        400
+      )
+    );
+  }
+  const postedBy = req.user._id;
+  const job = await Job.create({
+    title,
+    description,
+    category,
+    country,
+    city,
+    location,
+    fixedSalary,
+    salaryFrom,
+    salaryTo,
+    postedBy,
+  });
+  res.status(200).json({
+    success: true,
+    message: "Job Posted Successfully!",
+    job,
   });
 });
