54a84f6bb40a4c54ee412c1da2c27ce94eef3693
ranjansatvik <satvikranjan1@gmail.com>
2024-06-20 18:24:02 +0530

    added job functionality

--- backend/routes/jobRouter.js
+++ backend/routes/jobRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 import {
-  getAllJobs,
-  postJob,
-  getMyJobs,
-  updateJob,
   deleteJob,
+  getAllJobs,
+  getMyJobs,
+  getSingleJob,
+  postJob,
+  updateJob,
 } from "../controllers/jobController.js";
 import { isAuthorized } from "../middlewares/auth.js";
 
@@ -15,5 +16,6 @@ router.post("/post", isAuthorized, postJob);
 router.get("/getmyjobs", isAuthorized, getMyJobs);
 router.put("/update/:id", isAuthorized, updateJob);
 router.delete("/delete/:id", isAuthorized, deleteJob);
+router.get("/:id", isAuthorized, getSingleJob);
 
 export default router;
