3461d57df89b54f1760f0222eed7b0f77a43cdb2
ranjansatvik <satvikranjan1@gmail.com>
2024-06-20 17:36:46 +0530

    completed home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "",
+          "http://localhost:4000/api/v1/user/getUser",
           {
             withCredentials: true,
           }
